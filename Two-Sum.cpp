07fe00d5ff253bb66b5601e9dee09e93400935f0
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-14 15:19:47 +0530

    Update Two-Sum.cpp

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,65 +1,73 @@
 //true if there exists 2 numbers else false even if one number = target
 //use BSTIterator TC: O(N) SC: O(H)
 
-class BSTIterator 
-{
-  //reverse = true : before
-  //reverse = false : after
-  bool reverse = true;
-  private: stack<Node*> st;
-  public: 
-  BSTIterator(Node* root, bool isreverse)
-  {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator{
+public:
+
+    stack<TreeNode*> st;
+    bool reverse = true;
+    
+BSTIterator(TreeNode *root, bool isreverse){
     reverse = isreverse;
     pushAll(root);
-  }
-  
-  bool hasnext()
-  {
+}
+
+bool hasnext()
+{
     return !st.empty();
-  }
-  
-  int next()
-  {
-    Node* topnode = st.top();
+}
+    
+int next(){
+    TreeNode *topNode = st.top();
     st.pop();
-    if(reverse) pushAll(topnode->right);
-    else pushAll(topnode->right);
-    return topnode->val;
-  }
-  
-  private:
-  void pushAll(Node* node)
-  {
-    for(; node!=NULL;)
+    if (!reverse) pushAll(topNode->right);
+    else pushAll(topNode->left);
+
+    return topNode->val;
+}
+
+private:
+    void pushAll(TreeNode *node)
     {
-      st.push(node);
-      if(reverse) node = node->right;
-      else node = node->left;
+        for(; node != NULL; )
+        {
+            st.push(node);
+            if(reverse == true)
+                node = node->right;
+            else
+                node = node->left;
+        }
     }
-  }
-  
+
 };
 
-class solution
-{
-  public:
-  bool findtarget(Node* root, int k)
-  {
-    if(!root) return false;
-    BSTIterator l(root, false); //next
-    BSTIterator r(root, true); //before
+class Solution {
+public:
+bool findTarget(TreeNode* root, int k) {
+    if (root == NULL) return false;
+    BSTIterator l(root, false);  //next
+    BSTIterator r(root, true);  //before
     
     int i = l.next();
     int j = r.next();
     
-    while(i<j)
-    {
-      if(i+j == k) return true;
-      else if(i+j < k) i = l.next();
-      else j = r.next();
+    while (i < j){
+        if (i + j == k) return true;
+        else if (i + j < k) i = l.next();
+        else j = r.next();
     }
-    
     return false;
-  }
+}
 };
+
